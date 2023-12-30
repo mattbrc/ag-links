5ffdacfbdfebed09c43a320c46e2b324928b6caf
Matt <director@acidgambit.com>
2023-12-30 18:25:11 -0500

    add rest of event trackers

--- src/components/linktree.tsx
+++ src/components/linktree.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import Link from "next/link";
 import { AgAvatar } from "./ag-avatar";
 import { Button } from "./ui/button";
@@ -20,32 +19,57 @@ export function Linktree() {
       <div className="font-mono flex flex-col items-center justify-center mt-4">
         <p className="bg-zinc-700 px-1 rounded-lg">@ACIDGAMBIT</p>
         <div className="flex flex-col items-center justify-center mt-4">
-          <Button variant={"aglink"} asChild>
-            <Link
-              onClick={() => {
-                track("coaching");
-              }}
-              href="https://app.cal.com/acidgambit/30min"
-            >
+          <Button
+            onClick={() => {
+              track("coaching");
+            }}
+            variant={"aglink"}
+            asChild
+          >
+            <Link href="https://app.cal.com/acidgambit/30min">
               Coaching Intro Call (Free)
             </Link>
           </Button>
-          <Button variant={"aglink"} asChild>
+          <Button
+            onClick={() => {
+              track("discord");
+            }}
+            variant={"aglink"}
+            asChild
+          >
             <Link href="https://discord.gg/W4x3Eej95q">
               Official AG Discord
             </Link>
           </Button>
-          <Button variant={"aglink"} asChild>
+          <Button
+            onClick={() => {
+              track("phraks");
+            }}
+            variant={"aglink"}
+            asChild
+          >
             <Link href="https://acidgambit.gumroad.com/l/training">
               Get the AG Phraks Variant Program (Free)
             </Link>
           </Button>
-          <Button variant={"aglink"} asChild>
+          <Button
+            onClick={() => {
+              track("gumroad");
+            }}
+            variant={"aglink"}
+            asChild
+          >
             <Link href="https://acidgambit.gumroad.com/">
               Shop Training Programs
             </Link>
           </Button>
-          <Button variant={"aglink"} asChild>
+          <Button
+            onClick={() => {
+              track("substack");
+            }}
+            variant={"aglink"}
+            asChild
+          >
             <Link href="https://acidgambit.substack.com/">AG Writing</Link>
           </Button>
         </div>
