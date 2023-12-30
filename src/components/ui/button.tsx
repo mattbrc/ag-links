39d8aee4f883da908bd54e1c57ab3e22cd95d60d
Matt <director@acidgambit.com>
2023-12-30 16:08:17 -0500

    rm font weight

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         aglink:
-          "w-full sm:w-auto bg-primary text-primary-foreground hover:bg-primary/90 px-4 py-2 mb-4 font-bold",
+          "w-full sm:w-auto bg-primary text-primary-foreground hover:bg-primary/90 px-4 py-2 mb-4",
       },
       size: {
         default: "h-10 px-4 py-2",
