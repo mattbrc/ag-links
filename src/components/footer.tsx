98356046418b2c781f839e7163dd27bf1310fa39
Matt <director@acidgambit.com>
2024-01-14 21:28:13 -0500

    add ig icon

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,18 @@
 import { BsTwitterX } from "react-icons/bs";
+import { FaInstagram } from "react-icons/fa";
 import Link from "next/link";
 
 export function Footer() {
   return (
     <div className="p-2 m-4">
-      <Link href="https://twitter.com/acidgambit_">
-        <BsTwitterX />
-      </Link>
+      <div className="grid grid-flow-col items-center gap-4">
+        <Link href="https://twitter.com/acidgambit_">
+          <BsTwitterX />
+        </Link>
+        <Link href="https://instagram.com/acidgambit">
+          <FaInstagram size={20} />
+        </Link>
+      </div>
     </div>
   );
 }
