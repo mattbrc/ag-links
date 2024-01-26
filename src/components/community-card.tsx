ac288b3d90a8e5d4b18747f0d3363f5952257f93
Matt <director@acidgambit.com>
2024-01-26 10:20:39 -0500

    update community card

---
+++ src/components/community-card.tsx
@@ -0,0 +1,18 @@
+import { BsTwitterX } from "react-icons/bs";
+import { FaInstagram } from "react-icons/fa";
+import Link from "next/link";
+
+export function CommunityCard() {
+  return (
+    <div className="p-2 m-4">
+      <div className="grid grid-flow-col items-center gap-4">
+        <Link href="https://twitter.com/acidgambit_">
+          <BsTwitterX />
+        </Link>
+        <Link href="https://instagram.com/acidgambit">
+          <FaInstagram size={20} />
+        </Link>
+      </div>
+    </div>
+  );
+}
